2d6b01337d71f02d79175a21a1dddd430d9a7155
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-06-20 16:01:59 -0500

    api

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql');
- 
+
 const db = mysql.createPool({
-    connectionLimit: 100,
-    host:            'localhost',
-    user:            'root',
-    password:        '',
-    database:        'delivered',
-    debug:           false
+  connectionLimit: 100,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'delivered',
+  debug: false
 });
- 
-db.getConnection((err,connection)=> {
-    if(err)
+
+db.getConnection((err, connection) => {
+  if (err)
     throw err;
-    console.log('DB connected 0k');
-    connection.release();
-  });
-  
+  console.log('DB connected 0k');
+  connection.release();
+});
+
 module.exports = db;
 
 
--- server.js
+++ server.js
@@ -10,38 +10,13 @@ const logger = require('morgan');
 const cors = require('cors');
 const passport = require('passport');
 
-//traemos el paquete socket y lo inicializamos con el servidor
-const io = require('socket.io')(server);
-
-
-  const mercadopago = require('mercadopago');
-   
-  mercadopago.configure({
-        sandbox: true,
-        //prueba
-         access_token: 'TEST-3169176000815274-011902-493c596b84c9c44313b2af322d409570-1133725316'
-        //produccion
-      // access_token: 'APP_USR-3169176000815274-011902-c5a1554b6c735dbabcce47685b2d5b05-1133725316' 
-    });
-
-
-
-
 //Aqui se va a importar las rutas
 const usersrRoutes = require('./routes/userRoutes');
 const categoriesRoutes = require('./routes/categoriesRoutes');
 const productsRoutes = require('./routes/productsRoutes');
-const addressRoutes = require('./routes/addressRoutes');
-const ordersRoutes = require('./routes/ordersRoutes');
-const mercadoPagoRoutes = require('./routes/mercadoPagoRoutes');
 
 //**************************************** */
-
-//importamos los socket
-const ordersSocket = require('./sockets/ordersSockets');
-
 const multer = require('multer');
-
 
 const port = process.env.port || 3000;
 
@@ -50,7 +25,7 @@ app.use(logger('dev'));
 //parsear la respuesta que traemos en formato json
 app.use(express.json());
 app.use(express.urlencoded({
-  extended:true
+  extended: true
 }));
 
 app.use(cors());
@@ -61,44 +36,40 @@ require('./config/password')(passport);
 
 app.disable('x-powered-by');
 
-app.set('port',port);
+app.set('port', port);
 
- 
-//llamados a los socket
-ordersSocket(io);
+
+
 
 // const hostname = 'localhost';
 
 //imagenes 
-    //"socket.io": "^2.4.1"
+//"socket.io": "^2.4.1"
 const upload = multer({
-  storage:multer.memoryStorage()
+  storage: multer.memoryStorage()
 });
 
 
 //Aqui se va a importar las rutas le pasamos como parametro la aplicacion
-usersrRoutes(app,upload);
+usersrRoutes(app, upload);
 categoriesRoutes(app);
-productsRoutes(app,upload);
-addressRoutes(app);
-ordersRoutes(app);
-mercadoPagoRoutes(app);
+productsRoutes(app, upload);
+
 
 //********************************************** */
+server.listen(3000, '192.168.1.10' || 'localhost', function () {
+  console.log('http://localhost' + port + ' iniciada...');
 
-server.listen(3000,'192.168.1.8' || 'localhost', function(){
-    console.log('http://localhost'+port+ ' iniciada...');
-    
-}); 
+});
 //192.168.61.251:3000
 //ruta raiz
-app.get('/',(req, res) => {
-   res.send('Ruta raiz del backend');
+app.get('/', (req, res) => {
+  res.send('Ruta raiz del backend');
 });
 
 //MANEJO DE ERRORES
-app.use((err, req, res, next)=>{
-    res.status(err.status || 500).send(err.stack);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.stack);
 });
 
 module.exports = {
