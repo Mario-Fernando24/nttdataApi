9339ad4ea771f47cb13e6df5b12333fbf964f284
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-06-20 16:40:17 -0500

    remove jwt

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -3,9 +3,11 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-    app.get('/api/category/getAllCategory',passport.authenticate('jwt',{session:false})  ,categoriesController.getAllCategory);
-    //ruta para crear una categoria nueva
-    app.post('/api/category/create',passport.authenticate('jwt',{session:false})  ,categoriesController.create);
-    app.put('/api/category/update',passport.authenticate('jwt',{session:false})  ,categoriesController.updateCate);
+    // app.get('/api/category/getAllCategory',passport.authenticate('jwt',{session:false})  ,categoriesController.getAllCategory);
+    // app.post('/api/category/create',passport.authenticate('jwt',{session:false})  ,categoriesController.create);
+    // app.put('/api/category/update',passport.authenticate('jwt',{session:false})  ,categoriesController.updateCate);
+    app.get('/api/category/getAllCategory',categoriesController.getAllCategory);
+    app.post('/api/category/create',categoriesController.create);
+    app.put('/api/category/update',categoriesController.updateCate);
 
 }
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,12 +4,9 @@ const passport = require('passport');
 module.exports = (app,upload) => {
 
     //ruta para crear una categoria nueva
-    app.post('/api/product/create',passport.authenticate('jwt',{session:false}),upload.array('image',3),productsController.create);
-    app.get('/api/product/findByProducts/:id_category',passport.authenticate('jwt',{session:false})  ,productsController.findByProducts);
-    app.get('/api/product/findByProductSearch/:id_category/:name',passport.authenticate('jwt',{session:false})  ,productsController.findByProductSearch);
-
-
-    app.get('/api/product/getAllProducts',passport.authenticate('jwt',{session:false})  ,productsController.getAllProducts);
-
+    app.post('/api/product/create',upload.array('image',3),productsController.create);
+    app.get('/api/product/findByProducts/:id_category' ,productsController.findByProducts);
+    app.get('/api/product/findByProductSearch/:id_category/:name'  ,productsController.findByProductSearch);
+    app.get('/api/product/getAllProducts',productsController.getAllProducts);
 
 }
